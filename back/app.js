75c4efa2649aadab72ebc1d1ba7b0837e8cd6860
Lucas VC <lucasvc@hotmail.com>
2024-07-27 11:51:37 -0300

    aupdate app login

--- back/app.js
+++ back/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,18 +24,38 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'PW2021awqyeudj',
+  cookie: { maxAge: null },
+  resave: false,
+  saveUninitialized: true
+}))
+
+secured = async (req, res, next) => {
+  try {
+    console.log(req.session.id_usuario);
+    if (req.session.id_usuario) {
+      next();
+    } else {
+      res.redirect('/admin/login');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/admin/login' , loginRouter);
-app.use('/admin/novedades' , adminRouter);
+app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured, adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- back/routes/admin/login.js
+++ back/routes/admin/login.js
@@ -16,6 +16,10 @@ router.post('/', async (req, res, next) => {
     var data = await usuarioModel.getUserByUsernameAndPassword(usuario, password);
 
     if (data != undefined) {
+
+      req.session.id_usuario = data.id;
+      req.session.nombre = data.usuario;
+
       res.redirect('/admin/novedades');
     } else {
       res.render('admin/login', {
