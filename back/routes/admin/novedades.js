3ee813b1cc5644fefbe66101d80300fb0744d49e
Lucas VC <lucasvc@hotmail.com>
2024-07-27 12:14:10 -0300

    final update

--- back/routes/admin/novedades.js
+++ back/routes/admin/novedades.js
@@ -4,6 +4,7 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   res.render('admin/novedades', {
     layout: 'admin/layout',
+    persona: req.session.nombre
   });
 });
 
