3b3ce34e71300794c09f42f2bd8a50e59471d746
Lucas VC <lucasvc@hotmail.com>
2024-08-07 16:42:15 -0300

    modificar novedades

--- back/models/novedadesModel.js
+++ back/models/novedadesModel.js
@@ -24,4 +24,20 @@ async function deleteNovedadById(id) {
     return rows;
 }
 
-module.exports = { getNovedades, insertNovedad, deleteNovedadById }
+async function getNovedadById(id) {
+    var query = 'select * from novedades where id = ?';
+    var rows = await pool.query(query, [id]);
+    return rows[0];   
+}
+
+async function modificarNovedadById(obj, id) {
+    try {
+        var query = 'update novedades set ? where id=?';
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }  
+}
+
+module.exports = { getNovedades, insertNovedad, deleteNovedadById, getNovedadById, modificarNovedadById }
--- back/routes/admin/novedades.js
+++ back/routes/admin/novedades.js
@@ -39,12 +39,45 @@ router.post('/agregar', async (req, res, next) => {
       message: 'No se cargo la novedad'
     })
   }
-})
+});
 
 router.get('/eliminar/:id', async (req, res, next) => {
   var id = req.params.id;
   await novedadesModel.deleteNovedadById(id);
   res.redirect('/admin/novedades');
-})
+});
+
+router.get('/modificar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  // console.log(req.params.id);
+  var novedad = await novedadesModel.getNovedadById(id);
+
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  })
+});
+
+router.post('/modificar', async (req, res, next) => {
+  try {
+    var obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+    console.log(obj)
+
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+
+  } catch (error) {
+    console.log(error)
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se modifico la novedad'
+    })
+  }
+});
 
 module.exports = router;
