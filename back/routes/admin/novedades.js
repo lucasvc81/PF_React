b4c60ac8f4154a4574fd1d4a761fe76068d0ebda
Lucas VC <lucasvc@hotmail.com>
2024-08-08 07:54:53 -0300

    usuario - persona

--- back/routes/admin/novedades.js
+++ back/routes/admin/novedades.js
@@ -7,7 +7,7 @@ router.get('/', async function (req, res, next) {
   var novedades = await novedadesModel.getNovedades();
   res.render('admin/novedades', {
     layout: 'admin/layout',
-    usuario: req.session.nombre,
+    persona: req.session.nombre,
     novedades
   });
 });
